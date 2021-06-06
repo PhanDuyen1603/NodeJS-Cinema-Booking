b68903adec79255eb0de83129ab736f38fbda22a
Quang Vinh <quangvinhkhongcovo@gmail.com>
2021-06-07 01:11:05 +0700

    update home.js (thêm comment cho dễ nhìn, code thêm router.get('/:slug')... và tạo thêm string xuất ra thông báo rõ ràng hơn)

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,91 +1,107 @@
 const Router = require('express').Router;
 const fs = require('fs');
 const user = require('../models/User.js');
-const sendEmail  = require('../models/email.js');
+const sendEmail = require('../models/email.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const router = new Router();
 
 function Random() {
 	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-	for (var i = 0; i < 10; i++)
-	  text += possible.charAt(Math.floor(Math.random() * possible.length));
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	for (var i = 0; i < 6; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	return text;
 }
 
-router.get('/',function(req,res){
-	res.render('signUp.ejs');
+// [GET] /signup/
+router.get('/', function (req, res) {
+	res.render('auth/signup');
 });
-router.post('/',async function(req,res){
-	var text = Random();
-	var {user_Email ,user_Password ,user_Name,user_NumberPhone } = req.body ;
-	const User =  await user.findOne({
-		where : {
-			user_Email : user_Email ,
-		}
-	});
-	if(User){
-		res.render('signUp.ejs',{User});
+
+// [POST] /signup/
+router.post('/', async function (req, res) {
+	const code = Random();
+	const { user_Email, user_Password, user_Name, user_NumberPhone } = req.body;
+	const User = await user.findByEmail(user_Email);
+	const emailError = 'Tài khoản đã tồn tại.';
+	const phoneNumberError = 'Số điện thoại này đã được đăng ký.';
+
+
+	if (User) {
+		res.render('auth/signup', { emailError });
 	}
-	else{
-		const NumberPhone = await user.findOne({
-			where :{
-				user_NumberPhone : user_NumberPhone ,
+	else {
+		const phoneNumber = await user.findOne({
+			where: {
+				user_NumberPhone: user_NumberPhone,
 			}
 		});
-		if(NumberPhone)
-		{
-			res.render('signUp.ejs',{NumberPhone});
+		if (phoneNumber) {
+			res.render('auth/signup', { phoneNumberError });
 		}
-		else{
-			const hash = await bcrypt.hash(user_Password,saltRounds);
-			await	user.create({
-						user_Email ,
-						user_Password : hash,
-						user_Name ,
-						user_NumberPhone,
-						user_Address : "chưa cập nhật",
-						user_Code : text ,
-					}).then(async function(user){
-						const info = await sendEmail(req.body.user_Email, 'Xác nhận tài khoản', 'Đây là email tự động, vui lòng không gửi mail qua địa chỉ này.\nMã xác nhận của bạn: ' + text	);
-						res.render('loginConfirm.ejs',{user_Email});
-					}).catch(function(err){
-						console.log(err,req.body);
-					});
-			};
-		}
+		else {
+			const hashPassword = await bcrypt.hash(user_Password, saltRounds);
+
+			await user.create({
+				user_Email,
+				user_Password: hashPassword,
+				user_Name,
+				user_NumberPhone,
+				user_Address: "Chưa cập nhật",
+				user_Code: code,
+			}).then(async function (user) {
+				const info = await sendEmail(user_Email, '[MỘT CHÚT FILM] - XÁC THỰC TÀI KHOẢN', 'Content', `Đây là email tự động, vui lòng không gửi mail qua địa chỉ này.\nMã xác nhận của bạn: <b>${code}</b>`);
+
+				res.render('auth/signupConfirm', { user_Email });
+
+			}).catch(function (err) {
+				console.log(err, req.body);
+			});
+		};
+	}
 });
-router.post('/confirm/',async function(req,res, next){
-	const codefail="a"; 
-	const user_Email = req.body.email ;
-	const {user_ConfirmEmail} = req.body;
+
+router.get('/confirm', function (req, res) {
+	const user_Email = '';
+	res.render('auth/signupConfirm', { user_Email });
+});
+
+// [POST] /signup/confirm
+router.post('/confirm', async function (req, res, next) {
+	const invalidCode = "Mã xác thực không chính xác";
+	const invalidMail = "Tài khoản không hợp lệ";
+
+	const user_Email = req.body.email;
+	const code = req.body.code;
+
 	user.findOne({
 		where: {
-			user_Email:user_Email,
-			user_Code:user_ConfirmEmail,
+			user_Email: user_Email,
 		}
-	}).then(function(User){
-		if(User)
-		{
-			user.update({
-				accept_User: true,
-			},
-			{
-				where: {user_Email:user_Email,}
-			}).then(function(result){
-				req.session.user_Id = User.user_ID ;
-				res.redirect('/');
-			}).catch(next);
-		}
-		else
-		{
-			res.render('loginConfirm.ejs',{user_Email,codefail});
-		}
-	}).catch(function(next){
-		res.render('loginConfirm.ejs',{user_Email,codefail});
-	});
+	})
+		.then((User) => {
+			if (User.user_Code === code) {
+				user.update({
+					accept_User: true, user_Code: null
+				},
+					{
+						where: { user_Email: user_Email }
+					}).then(() => {
+						req.session.user_Id = User.user_ID;
+						res.redirect('/');
+					}).catch(next);
+			} else {
+				res.render('auth/signupConfirm', { user_Email, invalidCode });
+			}
+		}).catch(() => {
+			res.render('auth/signupConfirm', { user_Email, invalidMail });
+		});
 });
+
+router.get('/:slug', (req, res) => {
+	res.render('404NotFound');
+})
 
 module.exports = router;
