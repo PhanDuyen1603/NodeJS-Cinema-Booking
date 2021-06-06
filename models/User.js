d16d6b82037d7767f6f056dbc9f1d15fa88bb151
Quang Vinh <quangvinhkhongcovo@gmail.com>
2021-06-07 00:54:38 +0700

    update user.js

--- models/User.js
+++ models/User.js
@@ -2,41 +2,49 @@ const Sequelize = require('sequelize');
 const db = require('./db');
 
 const user = db.define('User', {
-	user_ID : {
-		type : Sequelize.INTEGER ,
-		primaryKey : true ,
-		allowNull : false,
-		autoIncrement :true ,
+	user_ID: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		allowNull: false,
+		autoIncrement: true,
 	},
-	user_Email : {
-		type : Sequelize.STRING ,
-		allowNull : false,
-		unique : true,
+	user_Email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true,
 	},
-	user_Password : {
-		type : Sequelize.STRING ,
-		allowNull : false ,
+	user_Password: {
+		type: Sequelize.STRING,
+		allowNull: false,
 	},
-	user_Name : {
-		type : Sequelize.STRING ,
-		allowNull : false ,
+	user_Name: {
+		type: Sequelize.STRING,
+		allowNull: false,
 	},
-	user_NumberPhone : {
-		type : Sequelize.STRING ,
-		allowNull : false ,		
+	user_NumberPhone: {
+		type: Sequelize.STRING,
+		allowNull: false,
 	},
-	user_Address :{
-		type : Sequelize.STRING ,
-		allowNull : true,
+	user_Address: {
+		type: Sequelize.STRING,
+		allowNull: true,
 	},
-	user_Code : {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-	accept_User:{
+	user_Code: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	accept_User: {
 		type: Sequelize.BOOLEAN,
 		defaultValue: false,
 	}
 });
 
-module.exports = user ;
+user.findByEmail = async (email) => {
+	return user.findOne({ where: { user_Email: email } });
+}
+
+user.activateAccount = async (email, code) => {
+	user.update();
+}
+
+module.exports = user;
