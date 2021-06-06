a34aa4a1bcf2f89cdd85f4173870b9553b6e1db6
Quang Vinh <quangvinhkhongcovo@gmail.com>
2021-06-07 00:52:46 +0700

    update email.js đổi email xác thực

--- models/email.js
+++ models/email.js
@@ -1,19 +1,19 @@
 const nodemailer = require('nodemailer');
 
 async function sendEmail(to, subject, text, html) {
-  
+
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-        user: '17k1.web2.demo@gmail.com',
-        pass: 'abcXYZ123~' 
+      user: 'motchutfilm@gmail.com',
+      pass: '123456Mc'
     },
   });
 
   const info = await transporter.sendMail({
-    from: '<17k1.web2.demo@gmail.com>',
+    from: 'motchutfilm@gmail.com',
     to: to,
     subject: subject,
     text: text,
