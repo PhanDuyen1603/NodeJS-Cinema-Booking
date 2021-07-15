32a36869e4bfd1d2de9a5c99ed3844cd5792d6ce
Quang Vinh <quangvinhkhongcovo@gmail.com>
2021-07-16 02:01:32 +0700

    up code

---
+++ models/db.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+const url = process.env.DATABASE_URL || 'postgres://postgres:0917196909vVINH@@localhost:5432/cinema2'
+const db = new Sequelize(url);
+
+module.exports = db;
